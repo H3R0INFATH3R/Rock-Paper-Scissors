31de507d5bff065fdd98594fa3740ae83d1f379b
Don <dagem.alemseged1@gmail.com>
2022-10-18 05:23:50 -0400

    Connect the game to a UI

--- js/script.js
+++ js/script.js
@@ -1,15 +1,61 @@
+/*
+    Written by: Dagem Woldeyohannes
+    Date: October 18, 2022
+    Rock Paper Scissors game against cpu with live score update.
+*/
+
+const rock = document.querySelector('.rock-btn');
+const paper = document.querySelector('.paper-btn');
+const scissors = document.querySelector('.scissors-btn');
+const liveResult = document.querySelector('.display-result');
+const result = document.createElement('p');
+result.classList.add('result');
+const score = document.createElement('h3');
+score.classList.add('score');
+const options = document.querySelector('.main-sec');
+const btn = document.querySelectorAll('.rock-btn');
+
+let computerScore = 0;
+let playerScore = 0;
+
 let getComputerChoice = () => {
     let rand = Math.floor(Math.random() * 3);
     let options = ['Rock', 'Paper', 'Scissors'];
     return options[rand];
 }
 
-let computerScore = 0;
-let playerScore = 0;
+let announceWinner = () => {
+    const finalWinner = document.createElement('h2');
+    finalWinner.classList.add('finalWinner');
+
+    if (playerScore === 5) {
+        finalWinner.textContent = 'Congratulations... You won!';
+        liveResult.appendChild(finalWinner);
+    }
+    else if (computerScore === 5) {
+        finalWinner.textContent = 'You lose... Try again?';
+        liveResult.appendChild(finalWinner);
+    }
+
+    const playAgain = document.createElement('button');
+    playAgain.classList.add('btn');
+    playAgain.classList.add('playAgain');
+    playAgain.textContent = 'Play again';
+    liveResult.appendChild(playAgain);
+
+    playAgain.addEventListener('click', function () {
+        playerScore = 0;
+        computerScore = 0;
+        liveResult.removeChild(result);
+        liveResult.removeChild(score);
+        liveResult.removeChild(finalWinner);
+        liveResult.removeChild(playAgain);
+    });
+}
 
 let playRound = (playerSelection, computerSelection = getComputerChoice()) => {
     if (playerSelection === computerSelection) {
-        console.log('Draw');
+        result.textContent = `Draw!`;
     }
     else if (
         (playerSelection == 'Rock' && computerSelection == 'Scissors') ||
@@ -17,30 +63,32 @@ let playRound = (playerSelection, computerSelection = getComputerChoice()) => {
         (playerSelection == 'Paper' && computerSelection == 'Rock') 
     ){
         playerScore++;
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        result.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        score.textContent = `${playerScore} - ${computerScore}`;
     }
     else {
         computerScore++;
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        score.textContent = `${playerScore} - ${computerScore}`;
+    }
+    
+    if (playerScore === 5 || computerScore === 5) {
+        announceWinner();
     }
 }
 
-let cpu = 0;
-let you = 0;
-
-let game = () => {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Rock Paper or Scissors?');
-        playRound(playerSelection);
-        console.log(playerScore + ' - ' + computerScore);
-    }
-
-    if (playerScore > computerScore)
-        console.log('You Win!');
-    else if (computerScore > playerScore)
-        console.log('You Lose!');
-    else
-        console.log('Draw!');
-}
-
-game();
+rock.addEventListener('click', function() {
+    playRound('Rock');
+    liveResult.appendChild(result);
+    liveResult.appendChild(score);
+});
+paper.addEventListener('click', function() {
+    playRound('Paper');
+    liveResult.appendChild(result);
+    liveResult.appendChild(score);
+});
+scissors.addEventListener('click', function () {
+    playRound('Scissors');
+    liveResult.appendChild(result);
+    liveResult.appendChild(score);
+});
